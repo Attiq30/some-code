282b2cde61f3536fec62513a12cd8654c39625b1
Attiq30 <attiqrehman130@gmail.com>
2024-01-29 09:20:46 +0500

    First commit

---
+++ first.py
@@ -0,0 +1,29 @@
+def attiq():
+    print("i am attiq ur rehman  a fresh graduate student and recently in lahore doing a course of three months of the language python with the frame work of Django"
+          )
+
+
+
+input= int (input("enter any number: "))
+    
+if input<10:
+    print("i can generate a list of less than 10 perpole ")
+
+elif input>10 and input<20:
+    print("i can generate more than 10 and less than 20 people  list ")
+
+else:
+    print("please enter number between 1 to 20")
+
+print("enter something")
+
+while input<10 :
+    print( "i am jarvis")
+    break
+
+
+attiq()
+
+
+
+
