a0a87ed2403166878a39f4feea0b6c28326ac500
Attiq30 <attiqrehman130@gmail.com>
2024-01-29 13:34:44 +0500

    shorten the code

--- cal.py
+++ cal.py
@@ -16,36 +16,35 @@ def div(n1, n2):
 
 while(True):
 
- number = int(input("enter 1 for addition ,enter 2 for subtraction, enter 3 for multiplication ,enter 4 for division, enter 5 to end the program : "))
+    number = int(input("enter 1 for addition ,enter 2 for subtraction, enter 3 for multiplication ,enter 4 for division, enter 5 to end the program : "))
+    if number==5:
+        break
 
- 
     
-
- if number== 1:
     a=int(input("enter first number: "))
     b=int(input("enter secoind number: "))
     
-    print(addition(a,b))
+        
 
- elif number== 2:
-    a=int(input("enter first number: "))
-    b=int(input("enter secoind number: "))
-    print(sub(a,b))
-    a=int(input("enter first number: "))
-    b=int(input("enter secoind number: "))
- elif number==3:
-    a=int(input("enter first number: "))
-    b=int(input("enter secoind number: "))
-    print(mul(a,b))
+    if number== 1:
+        
+        
+        print(addition(a,b))
 
- elif number ==4:
-    a=int(input("enter first number: "))
-    b=int(input("enter secoind number: "))
-    print(div(a,b))
- elif number==5:
-    break
- else:
-    print("enter number between 1 to 4")
+    elif number== 2:
+        
+        print(sub(a,b))
+        
+    elif number==3:
+        
+        print(mul(a,b))
+
+    elif number ==4:
+        
+        print(div(a,b))
+    
+    else:
+        print("enter number between 1 to 4")
 
 
 
